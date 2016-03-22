0f4c050d240bd067cddf2e80816c5b3712ba595f
eduhlira2 <eduardu.silva@hotmail.com>
2016-03-22 17:34:41 -0300

    Pulo do popeyer

---
+++ Assets/CharacterControl.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+	public Transform player;
+	public float jumpTime = 0.4f;
+	public float jumpDelay = 0.4f;
+	public bool jumped = false;
+	public float force;
+	public bool isGrounded = true;
+	public Transform ground;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		isGrounded = Physics2D.Linecast (this.transform.position, ground.position, 1 << LayerMask.NameToLayer ("Plataforma"));
+		OnMouseDown ();
+	}
+
+	/*void JumpCharacter()
+	{
+
+		isGrounded = Physics2D.Linecast (this.transform.position, ground.position, 1 << LayerMask.NameToLayer ("Plataforma"));
+		
+		if (gameObject.name == "Cenario") {
+			GetComponent<Rigidbody2D> ().AddForce (transform.up * force);
+			jumpTime = jumpDelay;
+			jumped = true;
+
+			jumpTime -= Time.deltaTime;
+		}
+
+		if (jumpTime <= 0 && isGrounded && jumped) {
+			jumped = false;
+		}
+
+	}*/
+
+	void OnMouseDown()
+	{
+		/*(Input.GetMouseButtonDown(0)*/
+		if (Input.GetMouseButtonDown(0) && isGrounded && !jumped) {
+
+				GetComponent<Rigidbody2D>().AddForce(transform.up * force);
+				jumpTime = jumpDelay;
+				jumped = true;
+			}
+
+			jumpTime -= Time.deltaTime;
+
+
+			if (jumpTime <= 0 && isGrounded && jumped) {
+				jumped = false;
+			}
+
+
+	
+ }
+}
